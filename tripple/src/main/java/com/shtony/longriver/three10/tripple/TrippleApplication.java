cfc52f8548d957b653a275b4125c1f532879dd58
居洪涛 <hongtao.ju@blockchain001.com>
2019-07-05 13:43:47 +0800

    add hibernate dependency

---
+++ tripple/src/main/java/com/shtony/longriver/three10/tripple/TrippleApplication.java
@@ -0,0 +1,13 @@
+package com.shtony.longriver.three10.tripple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrippleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrippleApplication.class, args);
+	}
+
+}
